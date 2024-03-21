abcc7ea59625da7c91e54a72b4d5b8f379b33f30
Jason <jason.aghedo@gmail.com>
2024-03-21 09:50:59 +0100

    fix: Vercel Redirects

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
 	return (
 		<nav className="w-full h-[167px] border-b-[0.5px] border-[#24262F] px-[54px] py-[48px] flex items-center justify-between">
@@ -14,7 +16,12 @@ const NavBar = () => {
 					<a href="/">FAQ</a>
 				</span>
 			</div>
-			<button className="bg-[#054C73] text-white font-bold px-[72px] py-[25px] text-center rounded-xl font-montserrat shadow-[0px_4px_4px_0px_#00000040]">SIGN IN</button>
+			<Link
+				to="/login"
+				className="bg-[#054C73] text-white font-bold px-[72px] py-[25px] text-center rounded-xl font-montserrat shadow-[0px_4px_4px_0px_#00000040]"
+			>
+				SIGN IN
+			</Link>
 		</nav>
 	);
 };
